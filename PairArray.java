dbfb20467c819a699cd46ed12e7bebbde4e3ad78
Priyanshu <pmaurya.dev@gmail.com>
2025-02-01 21:12:11 +0530

    New

---
+++ PairArray.java
@@ -0,0 +1,16 @@
+public class PairArray {
+    public static void pair(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                System.out.print("(" + curr + "," + arr[j] + ") ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        pair(arr);
+    }
+}
