1e83b6e34aabe226afd725e0b5d8a6b360317ba0
Priyanshu <pmaurya.dev@gmail.com>
2025-02-04 23:23:56 +0530

    add

---
+++ Test.java
@@ -0,0 +1,36 @@
+public class Test {
+
+    public static void Practice(int arr[], int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                System.out.println(mid);
+                return;
+            }
+
+            if (arr[start] <= arr[mid]) {
+                if (target >= arr[start] && target < arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (target > arr[mid] && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+
+            }
+        }
+        System.out.println(-1);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 4, 5, 6, 7, 0, 1, 2 };
+        int target = 2;
+        Practice(arr, target);
+    }
+}
