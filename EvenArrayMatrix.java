626393d1729eb8d86bd91acaebe4b2c67d98e3f6
Priyanshu <pmaurya.dev@gmail.com>
2025-02-07 12:00:54 +0530

    Rotation

--- EvenArrayMatrix.java
+++ EvenArrayMatrix.java
@@ -1,13 +1,13 @@
 public class EvenArrayMatrix {
     public static void main(String[] args) {
-        int start = 2;
+        int start = 1;
         int row = 3;
         int col = 3;
         int[][] arr = new int[row][col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 arr[i][j] = start;
-                start += 2;
+                start += 1;
             }
         }
 
@@ -18,11 +18,43 @@ public class EvenArrayMatrix {
             System.out.println();
         }
         System.out.println();
-        for (int j = 0; j < col; j++) {
-            for (int i = row - 1; i >= 0; i--) {
+
+        for (int i = 0; i < row; i++) {
+            for (int j = col - 1; j >= 0; j--) {
+                System.out.printf("%3d ", arr[j][i]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = row - 1; i >= 0; i--) {
+            for (int j = col - 1; j >= 0; j--) {
                 System.out.printf("%3d ", arr[i][j]);
             }
             System.out.println();
         }
+        // for (int j = 0; j < col; j++) {
+        System.out.println();
+
+        for (int j = col - 1; j >= 0; j--) {
+            for (int i = 0; i < row; i++) {
+                System.out.printf("%3d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.printf("%3d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        // for (int i = row - 1; i >= 0; i--) {
+        // System.out.printf("%3d ", arr[i][j]);
+        // }
+        // System.out.println();
+        // }
     }
 }
