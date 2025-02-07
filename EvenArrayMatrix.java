8b268504a20b1a71407d1e43fe3adb47f127f447
Priyanshu <pmaurya.dev@gmail.com>
2025-02-07 11:39:56 +0530

    Add

--- EvenArrayMatrix.java
+++ EvenArrayMatrix.java
@@ -1,16 +1,26 @@
 public class EvenArrayMatrix {
     public static void main(String[] args) {
-        int[][] arr = new int[3][5];
         int start = 2;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 5; j++) {
+        int row = 3;
+        int col = 3;
+        int[][] arr = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 arr[i][j] = start;
                 start += 2;
             }
         }
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print(arr[i][j] + "\t");
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.printf("%3d ", arr[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int j = 0; j < col; j++) {
+            for (int i = row - 1; i >= 0; i--) {
+                System.out.printf("%3d ", arr[i][j]);
             }
             System.out.println();
         }
--- SnakeMatrix.java
+++ SnakeMatrix.java
@@ -1,24 +1,25 @@
 public class SnakeMatrix {
 
     public static void main(String[] args) {
-        int[][] arr = new int[3][5];
         int start = 2;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 5; j++) {
+        int row = 4;
+        int col = 5;
+        int[][] arr = new int[row][col];
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 arr[i][j] = start;
                 start += 2;
             }
         }
-        for (int i = 0; i < 3; i++) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < 5; j++) {
-                    System.out.print(arr[i][j] + " ");
-                }
-            } else {
-                for (int j = 4; j >= 0; j--) {
-                    System.out.print(arr[i][j] + " ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 || i == row - 1 || j == 0 || j == col - 1) {
+                    System.out.printf("%3d ", arr[i][j]);
+                } else {
+                    System.out.print("    ");
                 }
             }
+            System.out.println();
         }
     }
 }
