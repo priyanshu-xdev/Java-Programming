698c07ce351eedad4f3232199e2a90ed9bca1c1b
Priyanshu <pmaurya.dev@gmail.com>
2025-02-11 20:22:04 +0530

    add

--- Practice.java
+++ Practice.java
@@ -9,7 +9,7 @@ public class Practice {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if (arr[i][j] == 6) {
+                if (arr[i][j] == key) {
                     count++;
                 }
             }
