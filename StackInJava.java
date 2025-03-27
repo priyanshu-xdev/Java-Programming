0630531924a2286cbe3f6a53c846861b655b26c8
Priyanshu <pmaurya.dev@gmail.com>
2025-03-27 19:45:23 +0530

    Auto-commit

---
+++ StackInJava.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class StackInJava {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(4);
+        s.push(3);
+        s.push(2);
+
+        s.pop();
+        
+        System.out.println(!s.isEmpty());
+        System.out.println(s);
+    }
+}
